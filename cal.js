188c5a56ccc8295d004a2a460d222d272e3edd68
whuey79 <wdhuey@gmail.com>
2014-06-18 17:36:36 -0700

    add number days, month name, input

--- cal.js
+++ cal.js
@@ -9,13 +9,34 @@ function cal() {
     var todayYear = d.getFullYear();  // 2014
 }
 
-function drawCal() {
+function drawCal(month,firstDay) {
     var daysOfWeek = ['Su','Mo','Tu','We','Th','Fr','Sa'];
     var let = ['a','b','c','d','e','f'];
     var text = "";
     var outline = [];
+    var maxDaysOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+    var d = new Date();
 
-    text += "<table><tr>";
+    var monthName = ['January','February',
+                'March','April','May','June',
+                'July','August','September',
+                'October','November','December'];
+   
+    if ( typeof month !== 'undefined' ) {
+      d.setMonth(month);
+    }
+    if ( typeof firstDay !== 'undefined' ) {
+      d.setDate(firstDay);
+    }
+    else {
+      d.setDate(1);
+    }
+    
+    var firstOfTheMonth = d.getDay();
+    var maxCounter = maxDaysOfMonth[ d.getMonth() ];
+    var counter = 1;
+    
+    text += "<table id='cal'><caption>"+ monthName[d.getMonth()] +"</caption><tr>";
     // creates the labels for the week
     for (var i = 0; i<7; i++) {
          text += "<th id='dayLabel" + i + "'>" + daysOfWeek[i] + "</th>\n";
@@ -27,7 +48,13 @@ function drawCal() {
     for (var j = 0; j < let.length; j++) {
         text = "<tr>\n";
         for (var i = 0; i<7; i++) {
-          text += "<td id='" + let[j] +i +"'>X</td>\n"
+          if (( i < firstOfTheMonth && j == 0 ) || ( counter > maxCounter )) {
+            text += "<td id='" + let[j] +i +"'>-</td>\n";
+          }
+          else {
+            text += "<td id='" + let[j] +i +"'>" + counter + "</td>\n";
+            counter++;
+          }
         }
         text += "</tr>";
         outline.push(text);
