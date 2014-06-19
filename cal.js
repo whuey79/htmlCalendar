327f74ed89bacab483697df92cd5b1d58299da2c
Will Huey <wdhuey@gmail.com>
2014-06-18 20:31:57 -0700

    cleanup

--- cal.js
+++ cal.js
@@ -1,60 +1,63 @@
 
-function cal() {
-    var numDaysOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
-//    console.log(numDaysOfMonth);   
-    var d = new Date();
-    var todayDate = d.getDate();  // returns 10 ie. the 10th
-    var todayDayOfWeek = d.getDay();  // returns 2 ie. Tues
-    var todayMonth = d.getMonth();    // returns 5 = June
-    var todayYear = d.getFullYear();  // 2014
-}
 
-function drawCal(month,firstDay) {
-    var daysOfWeek = ['Su','Mo','Tu','We','Th','Fr','Sa'];
-    var let = ['a','b','c','d','e','f'];
-    var text = "";
+function drawCal(month, firstDay) {
+    'use strict';
+    var daysOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+    var letter = ['a','b','c','d','e','f'];
     var outline = [];
     var maxDaysOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
-    var d = new Date();
-
+    var text = "";
+   
     var monthName = ['January','February',
                 'March','April','May','June',
                 'July','August','September',
                 'October','November','December'];
-   
+
+    var d = new Date();
+                
     if ( typeof month !== 'undefined' ) {
-      d.setMonth(month);
+        d.setMonth(month);
     }
     if ( typeof firstDay !== 'undefined' ) {
-      d.setDate(firstDay);
+        d.setDate(firstDay);
     }
     else {
-      d.setDate(1);
+        d.setDate(1);
     }
     
-    var firstOfTheMonth = d.getDay();
-    var maxCounter = maxDaysOfMonth[ d.getMonth() ];
-    var counter = 1;
+    var firstOfTheMonth = d.getDay();  // 0-6
+    var dayCounter = 1;
+    var maxDayCounter;
+    var thisYear = d.getFullYear();  // 2014
+
+    if ( thisYear%4 == 0) {  // increment feb if leap year
+        maxDaysOfMonth[1] += 1;  
+    }
+
+    maxDayCounter = maxDaysOfMonth[ month ];
     
-    text += "<table id='cal'><caption>"+ monthName[d.getMonth()] +"</caption><tr>";
+    text += "<table id='cal'><caption>"+ monthName[month] + " " + thisYear + "</caption><tr>";
     // creates the labels for the week
-    for (var i = 0; i<7; i++) {
-         text += "<th id='dayLabel" + i + "'>" + daysOfWeek[i] + "</th>\n";
+    for (var i = 0; i<daysOfWeek.length; i++) {
+        text += "<th id='dayLabel" + i;
+        text += i == 0 || i == 6 ? "' class='weekend'>" : "' class='weekday'>";
+        text += daysOfWeek[i] + "</th>\n";
     }
 
     text += "</tr>";
     outline.push(text);
 
-    for (var j = 0; j < let.length; j++) {
+    // create the main calendar body
+    for (var j = 0; j < letter.length && dayCounter <= maxDayCounter; j++) {
         text = "<tr>\n";
-        for (var i = 0; i<7; i++) {
-          if (( i < firstOfTheMonth && j == 0 ) || ( counter > maxCounter )) {
-            text += "<td id='" + let[j] +i +"'>-</td>\n";
-          }
-          else {
-            text += "<td id='" + let[j] +i +"'>" + counter + "</td>\n";
-            counter++;
-          }
+        for (var i = 0; i< daysOfWeek.length; i++) {
+            if (( i < firstOfTheMonth && j == 0 ) || ( dayCounter > maxDayCounter )) {
+                text += "<td id='" + letter[j] +i +"'>-</td>\n";
+            }
+            else {
+                text += "<td id='" + letter[j] +i +"'>" + dayCounter + "</td>\n";
+                dayCounter++;
+            }
         }
         text += "</tr>";
         outline.push(text);
