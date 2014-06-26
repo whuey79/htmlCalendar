fb36a944ce6cf81a1d74edde4b3a7032cacb6b4e
Will Huey <wdhuey@gmail.com>
2014-06-25 21:47:07 -0700

    add round borders,default options

--- cal.js
+++ cal.js
@@ -1,36 +1,42 @@
 // 
 
-function drawCal(month) {
+function drawCal(options) {
     'use strict';
     var daysOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
     var letter = ['a','b','c','d','e','f'];
-    var outline = [];
-    var maxDaysOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
-    var text = "";
-   
+    var maxDaysOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31];  
     var monthName = ['January','February',
                 'March','April','May','June',
                 'July','August','September',
                 'October','November','December'];
 
-    var d = new Date();
+    var outline = [];
+    var text = "";
+
+    var d = new Date();    
+
+    var defaults = { 
+        "month":d.getMonth()
+    };  // default today
+    
+    //var options = options || {};
+    var month = (typeof options !== "undefined" ) ? options.month : defaults.month; 
+    
+    d.setMonth(month);    
     d.setDate(1);
     
-    if ( typeof month !== 'undefined' ) {
-        d.setMonth(month);
-    }
-    
-    var firstOfTheMonth = d.getDay();  // 0-6
+    var firstOfTheMonth = d.getDay();  // returns 0-6 to begin calendar
     var dayCounter = 1;
     var maxDayCounter;
     var thisYear = d.getFullYear();  // 2014
 
-    if ( thisYear%4 == 0) {  // increment feb if leap year
+    if ( thisYear%4 == 0) {  // increment Feb maxDays for leap year
         maxDaysOfMonth[1] += 1;  
     }
 
     maxDayCounter = maxDaysOfMonth[ month ];
     
+    // creates the table and thead
     text += "<table id='cal'><caption>"+ monthName[month] + " " + thisYear + "</caption><tr>";
     // creates the labels for the week
     for (var i = 0; i<daysOfWeek.length; i++) {
@@ -42,7 +48,7 @@ function drawCal(month) {
     text += "</tr>";
     outline.push(text);
 
-    // create the main calendar body
+    // create the main calendar tbody
     for (var j = 0; j < letter.length && dayCounter <= maxDayCounter; j++) {
         text = "<tr>\n";
         for (var i = 0; i< daysOfWeek.length; i++) {
@@ -57,6 +63,7 @@ function drawCal(month) {
         text += "</tr>";
         outline.push(text);
     }
+    // close table tag
     outline.push("</table>");
     return( outline.join('\n') );
 }
